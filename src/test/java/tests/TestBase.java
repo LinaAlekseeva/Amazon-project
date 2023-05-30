ff8f694ca1acfddaafdce361b06d8ae00fa94f2f
LinaAlekseeva <106020410+LinaAlekseeva@users.noreply.github.com>
2023-05-30 14:00:27 +0300

    + clearBrowserCookies

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -2,14 +2,12 @@ package tests;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
-import com.codeborne.selenide.WebDriverRunner;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
-import org.openqa.selenium.Cookie;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import pages.AuthorizationPage;
 import pages.RegistrationPage;
@@ -23,12 +21,11 @@ public abstract class TestBase {
     @BeforeAll
     static void beforeAll() {
         Configuration.baseUrl = System.getProperty("baseUrl", "https://www.amazon.com/");
+        Selenide.clearBrowserCookies();
         Configuration.browserSize = System.getProperty("browserSize", "1920x1080");
         Configuration.browser = System.getProperty("browser", "chrome");
         Configuration.browserVersion = System.getProperty("browserVersion", "100.0");
         Configuration.remote = System.getProperty("remote", "https://user1:1234@selenoid.autotests.cloud/wd/hub");
-        Cookie ck = new Cookie("langcookie", "ru");
-        WebDriverRunner.getWebDriver().manage().addCookie(ck);
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("enableVNC", true);
         capabilities.setCapability("enableVideo", true);
