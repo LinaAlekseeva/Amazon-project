6bf957fb7715472980010fa80ddadb589d0ab9c7
LinaAlekseeva <106020410+LinaAlekseeva@users.noreply.github.com>
2023-06-05 14:33:50 +0300

    +first commit

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -21,6 +21,8 @@ public class TestBase {
         WebDriverConfig config = ConfigFactory.create(WebDriverConfig.class, System.getProperties());
 
         Configuration.baseUrl = config.getBaseUrl();
+        Configuration.baseUrl = config.getBaseUrl();
+        Configuration.baseUrl = config.getBaseUrl();
         Configuration.browser = config.getBrowser();
         Configuration.browserVersion = config.getBrowserVersion();
 
