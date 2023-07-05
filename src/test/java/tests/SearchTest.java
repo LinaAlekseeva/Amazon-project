19604197cd02aba5b0f39cb31978fb5f83e1916b
LinaAlekseeva <106020410+LinaAlekseeva@users.noreply.github.com>
2023-07-05 15:26:03 +0300

    +Search test

--- src/test/java/tests/SearchTest.java
+++ src/test/java/tests/SearchTest.java
@@ -1,13 +1,11 @@
 package tests;
 
 import io.qameta.allure.*;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Tag;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
+import static com.codeborne.selenide.Selenide.open;
 import static io.qameta.allure.Allure.step;
 @Tag("Search")
 @Epic("Search and adding to basket ")
@@ -18,16 +16,19 @@ import static io.qameta.allure.Allure.step;
 public class SearchTest extends TestBase{
     @BeforeEach
     public void beforeEach() {
-        searchPage.openPage();
+        open("https://www.amazon.com");
     }
-    @ParameterizedTest(name = "{arguments}")
-    @CsvSource(value = {
-            "Health & Household | Vitamin B12",
-            "Books | Fyodor Dostoyevsky: The Complete Novels",
-            "Home & Kitchen | Blender",
-            "Baby | Pacifier ",
-    }, delimiter = '|')
-    void SearchTestForm(String itemType, String itemName) {
+    @ParameterizedTest
+    @CsvSource({
+            "Health&Household, Vitamin B12",
+            "Books, Fyodor Dostoyevsky: The Complete Novels",
+            "Home&Kitchen, Blender",
+            "Baby, Pacifier "})
+
+     @Tags({@Tag("BLOCKER"), @Tag("UI_TEST")})
+      void searchTestForm (
+              String itemType,
+              String itemName) {
         Allure.getLifecycle().updateTestCase(test ->
                         test.setName("An existing item can be found with filter: [Filter, Search text]"));
         step("Заполнить поля формы и нажать кнопку", () -> {
--- src/test/java/tests/TitleCheck.java
+++
@@ -1,4 +0,0 @@
-package tests;
-
-public class TitleCheck extends TestBase {
-}
