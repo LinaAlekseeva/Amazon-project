f5dc8e3b295618ac7979c2eb196f825458098502
LinaAlekseeva <106020410+LinaAlekseeva@users.noreply.github.com>
2023-07-22 16:42:19 +0300

    + change

--- src/test/java/tests/SearchTest.java
+++ src/test/java/tests/SearchTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.*;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
-import static com.codeborne.selenide.Selenide.open;
 import static io.qameta.allure.Allure.step;
 
 @Tag("search")
