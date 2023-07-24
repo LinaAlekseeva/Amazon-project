52119e53fa31b4aabac6c29f4ec7100616b58b29
LinaAlekseeva <106020410+LinaAlekseeva@users.noreply.github.com>
2023-07-24 17:21:36 +0300

    + change

--- src/test/java/tests/SearchTest.java
+++ src/test/java/tests/SearchTest.java
@@ -38,6 +38,7 @@ public class SearchTest extends TestBase {
         step("Selecting the found product and adding it to the List", () -> {
             searchPage.choice()
                     .addToList();
+            authorizationPage.authorization(email, password);
         });
         step("Request Validation", () -> {
             searchPage.verifyResultsAll();
