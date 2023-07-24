00d1c7fd6d4da1ad639e2917953113565a3f5099
LinaAlekseeva <106020410+LinaAlekseeva@users.noreply.github.com>
2023-07-24 13:15:54 +0300

    + change

--- src/test/java/config/RemoteConfig.java
+++ src/test/java/config/RemoteConfig.java
@@ -1,19 +1,15 @@
 package config;
 
 import org.aeonbits.owner.Config;
-@Config.Sources({"classpath:selenoid.properties"})
 
 public interface RemoteConfig extends Config{
 
     @Config.Key("selenoid.url")
-    @DefaultValue("selenoid.autotests.cloud")
     String url();
 
     @Config.Key("selenoid.login")
-    @DefaultValue("user1")
     String login();
 
     @Config.Key("selenoid.password")
-    @DefaultValue("user1")
     String password();
 }
